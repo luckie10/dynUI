efedb67e9cf8656599b8aecef40f9cc86e0ab011
Boban Kilvin <btrajcevski10@gmail.com>
2023-05-30 04:15:12 -0400

    slideshow: Add assets

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import cat1 from "./pexels-cristyan-bohn-4012470.jpg";
+import cat2 from "./pexels-pixabay-236648.jpg";
+import cat3 from "./pexels-jenna-hamra-915060.jpg";
+import cat4 from "./pexels-joice-rivas-11777956.jpg";
+import cat5 from "./pexels-julissa-helmuth-5690400.jpg";
+import cat6 from "./pexels-alina-levkovich-16450166.jpg";
+
+export const cats = {
+  cat1,
+  cat2,
+  cat3,
+  cat4,
+  cat5,
+  cat6,
+};
